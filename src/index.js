ae4b2de8587ae88baea5aacbc8fd409ebf39ad19
Asyoune <https://github.com/Asyoune/My-repository.git>
2026-06-11 11:26:53 +0700

    Update React component

--- src/index.js
+++ src/index.js
@@ -5,11 +5,7 @@ import Car from './Car';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Constructor from './Constructor';
-import Props from './Props';
 import PropConstructor from './PropConstructor';  
-import Garage from './ComponentInComponent';
-import State from './State.jsx';
-import ChangingState from './ChangingState.jsx';
 import Foo from './Foo.js';
 import FixedFoo from './FixedFoo.js';
 import Counter from './Counter.js';
@@ -21,13 +17,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <Car />  */}
-    {/* <Props color="blue" /> */}
     {/* <Constructor /> */}
     {/* <PropConstructor model ="Mustang" /> */}
-    {/* <Garage /> */}
     {/* <App /> */}
-    {/* <State /> */}
-    {/* <ChangingState /> */}
     {/* <Foo /> */}
     {/* <FixedFoo /> */}
     {/* <Counter /> */}
